f1686a8e9657a588eb6ac19e11e6218519de47ec
harshalpatil2809 <harshalpatil28095@gmail.com>
2025-09-04 22:14:41 +0530

    create add layout

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ tasks = []
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",tasks = tasks)
 
 @app.route("/add", methods=['POST'])
 def submit():
