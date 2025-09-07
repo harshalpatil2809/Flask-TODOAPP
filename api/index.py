674804e288de879d9c90bf10643553ea2ec495e5
harshalpatil2809 <harshalpatil28095@gmail.com>
2025-09-07 17:15:27 +0530

    working on deploy 3

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,send_from_directory
 
-app = Flask(__name__, template_folder="./templates",static_folder="./static")
+app = Flask(__name__, template_folder="../templates",static_folder="../static")
 
 tasks = []
 
